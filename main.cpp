d607293290cf48104b839cf1874d85616e80a8dd
pzalichta <zalichta@poczta.onet.pl>
2020-03-20 18:51:28 +0100

    wersja beta

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
+
     cout << "Hello, World!" << endl;
+
+    string imie, nazwisko;
+    cout << "Wpisz swoje imie: "; cin >> imie;
+    cout << "Wpisz swoje nazwisko: "; cin >> nazwisko;
+    cout << "Witaj " << imie << " " << nazwisko << endl;
     return 0;
 }
