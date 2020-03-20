6781df2b56f75ced0c5a5138125f9e584b9684fc
pzalichta <zalichta@poczta.onet.pl>
2020-03-20 18:46:30 +0100

    wersja alpha nowa funkcjonalnosc

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    string imie, nazwisko;
+    cout << "Wpisz swoje imie: "; cin >> imie;
+    cout << "Wpisz swoje nazwisko: "; cin >> nazwisko;
+    cout << "Witaj " << imie << " " << nazwisko << endl;
+    cout << "Twoje imie i nazwisko zawiera " << (imie.length() + nazwisko.length()) << "liter";
     return 0;
 }
